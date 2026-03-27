f313ae866d1fa432e2e89c0e792fdad5ac544183
Jamari Scott Robinson <jamariscott@gmail.com>
2026-03-26 22:03:23 -0400

    Fix CORS for Vercel

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+	"http://localhost:5173",
+	"https://profile-site-navy.vercel.app"
+    ],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
